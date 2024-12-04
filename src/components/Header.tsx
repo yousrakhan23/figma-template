a540186a6eda9f2e94ffa5a5c95b4f12ac537e12
yousrakhan23 <ammarahkhan2016@gmail.com>
2024-12-05 00:17:34 +0500

    header done

---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+// import React from 'react'
+
+// const Header = () => {
+//   return (
+//     <div>
+//         <header className='w-1440 h-48 '>
+//             <h3 className='bg-black text-white text-center p-2 w-550'>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! 
+//                 <span className='m-5 underline'>ShopNow
+//                     </span>
+//                     <p className='float-right mr-5'>English</p>
+//                     </h3>
+//                     </header>
+
+//     </div>
+//   )
+// }
+
+// export default Header;
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div>
+            <header className="w-1440 h-48 ">
+                <h3 className="text-center bg-black text-white p-2 w-550">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+                    <span className="underline mx-3">ShopNow</span>
+                    <span className="ml-[150px]">English 
+                        <svg width="21" height="10" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg " className='ml-[1180px] '>
+                        <g filter="url(#filter0_d_17807_19)">
+                            <path d="M10.364 4.95L15.314 0L16.728 1.414L10.364 7.778L4.00003 1.414L5.41403 0L10.364 4.95Z" fill="white" />
+                        </g>
+                        <defs>
+                            <filter id="filter0_d_17807_19" x="0" y="0" width="20.728" height="15.778" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                                <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                                <feOffset dy="4" />
+                                <feGaussianBlur stdDeviation="2" />
+                                <feComposite in2="hardAlpha" operator="out" />
+                                <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_17807_19" />
+                                <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_17807_19" result="shape" />
+                            </filter>
+                        </defs>
+                    </svg>
+
+                    </span>
+                </h3>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
